969ad8192117033e932c46d3bab2695f12faedcf
Anjana Sivadas <118110476+anjanasivadas@users.noreply.github.com>
2026-03-22 18:35:25 +0530

    Fix formatting issues in Projects.tsx

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -7,7 +7,7 @@ const projects = [
       "A full-stack web application for managing and browsing books with features like add, update, delete, and cart functionality. Built using Django and SQLite.",
     github: "https://github.com/anjanasivadas/BookCart__2026.git", 
     live: "https://anjanasivadas.pythonanywhere.com/"
-  }
+  },
   {
     title: "WeatherApp247",
     description:
